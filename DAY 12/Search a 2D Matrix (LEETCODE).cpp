a424310f04c40ae8849e138ce491300216fcdd7f
Abhishek Rout <64718836+Abhishek-Rout@users.noreply.github.com>
2020-10-16 22:14:32 +0530

    Add files via upload

---
+++ DAY 12/Search a 2D Matrix (LEETCODE).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.size() == 0 || matrix[0].size() == 0) return false;
+        int i = matrix.size() -1;
+        int j = matrix[0].size() -1;
+        if(target > matrix[i][j] || target < matrix[0][0] ) return false;
+
+        int x = 0;
+        int y = j;
+        while(x <= i&& y>=0){
+            if(target == matrix[x][y]) return true;
+            else if( target < matrix[x][y]){
+                y--;
+            }else{
+                x++;
+            }
+        }
+        return false;
+    }
+};
