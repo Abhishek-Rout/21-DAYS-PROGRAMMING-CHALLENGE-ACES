0020a0647f47501ca18484bf2d71b9f304f79ee4
Abhishek Rout <64718836+Abhishek-Rout@users.noreply.github.com>
2020-10-17 20:00:56 +0530

    Add files via upload

---
+++ DAY 13/Repeated DNA Sequences (LEETCODE).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        vector<string> ans;
+        
+        int key = 0;
+        unsigned hashTable[1<<20] = {0}; 
+        for(int i=0; i<s.length();i++){
+            
+            key = (key << 2 | (s[i]+1)%5) & 0xfffff;
+            if(i < 9) continue;
+            
+            if(hashTable[key] == 1){
+                ans.push_back(s.substr(i-9,10));
+            }
+            
+            hashTable[key]++;
+        }
+        
+        return ans;
+    }
+};
