eecb158d5c4c99748a8234aa418f4c0af07ab786
Abhishek Rout <64718836+Abhishek-Rout@users.noreply.github.com>
2020-10-21 15:55:20 +0530

    Add files via upload

---
+++ DAY 17/Asteroid Collision (LEETCODE).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> res;
+        for(int a: asteroids) {
+            if(a > 0)
+                res.push_back(a);
+            else{
+                while(!res.empty() && res.back() > 0 && res.back() + a < 0)
+                    res.pop_back();
+                if(res.empty() || res.back() < 0)
+                    res.push_back(a);
+                else if(res.back() + a == 0)
+                    res.pop_back();
+            }
+        }
+        return res;
+    }
+};
