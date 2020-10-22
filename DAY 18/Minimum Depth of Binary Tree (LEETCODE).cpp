e50f5acc3a1415976ae08380ce1d34607aee8ff3
Abhishek Rout <64718836+Abhishek-Rout@users.noreply.github.com>
2020-10-22 22:16:46 +0530

    Add files via upload

---
+++ DAY 18/Minimum Depth of Binary Tree (LEETCODE).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+class Solution {
+public:
+    int minDepth(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if(root == NULL){
+            return 0;
+        }
+        if(root->left == NULL && root->right != NULL){
+            return minDepth(root->right) + 1;
+        }
+        else if(root->right == NULL && root->left != NULL){
+        	return minDepth(root->left) + 1;
+        }
+        else{
+        	int left = minDepth(root->left) + 1;
+        	int right = minDepth(root->right) + 1;
+        	return (left < right ? left : right);
+        }
+    }
+};
