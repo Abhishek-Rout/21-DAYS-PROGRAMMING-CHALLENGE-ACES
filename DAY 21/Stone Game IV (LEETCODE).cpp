89f9b52cd4cd8542101e18d1266794070e8773fc
Abhishek Rout <64718836+Abhishek-Rout@users.noreply.github.com>
2020-10-25 21:00:59 +0530

    Add files via upload

---
+++ DAY 21/Stone Game IV (LEETCODE).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+  bool winnerSquareGame(int n) {    
+    vector<int> cache(n + 1, 0); 
+    function<int(int)> win = [&](int n) -> int {
+      if (n == 0) return -1;
+      if (cache[n]) return cache[n];
+      for (int i = sqrt(n); i >= 1; --i)
+        if (win(n - i * i) < 0) return cache[n] = 1;      
+      return cache[n] = -1;
+    };
+    return win(n) > 0;
+  }
+};
